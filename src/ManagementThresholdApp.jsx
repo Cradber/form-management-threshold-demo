d2eec919a6e89c22f45ace76c2979ebe2124b8ae
yankie <yakielarapacheco@yahoo.com>
2022-01-17 18:51:53 -0500

    Adding style for wrapper application

--- src/ManagementThresholdApp.jsx
+++ src/ManagementThresholdApp.jsx
@@ -1,4 +1,14 @@
 
+const Style = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#eeeeee',
+    height: '97.7vh',
+    margin: 0,
+    borderRadius: '5px'
+}
+
 const ManagementThresholdApp = () => (
     <h1>Management threshold form</h1>
 )
