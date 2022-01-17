a205a4df2150ec41c5a681d46c94472d3d2d07cc
yankie <yakielarapacheco@yahoo.com>
2022-01-17 18:53:36 -0500

    Adding style form component

--- src/ManagementThresholdApp.jsx
+++ src/ManagementThresholdApp.jsx
@@ -8,6 +8,12 @@ const Style = {
     margin: 0,
     borderRadius: '5px'
 }
+const BoxStyle = {
+    width: '60%',
+    padding: '1.5rem 2rem',
+    borderRadius: '5px',
+    background: 'white'
+}
 
 const ManagementThresholdApp = () => (
     <h1>Management threshold form</h1>
