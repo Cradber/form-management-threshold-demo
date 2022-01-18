9a21c4ca21acd0836809187c94c18b21a7e54223
yankie <yakielarapacheco@yahoo.com>
2022-01-17 23:02:51 -0500

    Adding prop-types check to SelectorColor component

--- src/components/SelectorColor.jsx
+++ src/components/SelectorColor.jsx
@@ -1,5 +1,5 @@
 import {Field} from "react-final-form";
-
+import PropTypes from "prop-types";
 
 export const SelectorColor = ({name, color = '#000000'}) => (
     <Field name={name}
@@ -16,3 +16,12 @@ export const SelectorColor = ({name, color = '#000000'}) => (
         }
     </Field>
 )
+
+SelectorColor.propTypes = {
+    name: PropTypes.string.isRequired,
+    color: (props, propName) => {
+        if (typeof props[propName] !== 'string' || !props[propName].toString().match(/#[0-9a-fA-F]{6}/)) {
+            return new Error(`The specified value ${props[propName]} does not conform to the required format. The format is "#rrggbb" where rr, gg, bb are two-digit hexadecimal numbers.`)
+        }
+    }
+}
