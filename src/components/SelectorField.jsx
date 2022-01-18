654e172a3ff13ad9196bcf3f8d69a7b3f2f2d504
yankie <yakielarapacheco@yahoo.com>
2022-01-17 23:02:07 -0500

    Adding prop-types check to OptionSelector component

--- src/components/SelectorField.jsx
+++ src/components/SelectorField.jsx
@@ -1,9 +1,16 @@
 import {Field} from "react-final-form";
 import {FormControl, InputLabel, NativeSelect} from "@mui/material";
+import PropTypes from "prop-types";
 
 export const OptionSelector = ({value, children}) => (
     <option value={value}>{children}</option>
 )
+
+OptionSelector.propType = {
+    value: PropTypes.string.isRequired,
+    children: PropTypes.any.isRequired
+}
+
 
 export const SelectorField = ({name, placeholder, children}) => {
 
