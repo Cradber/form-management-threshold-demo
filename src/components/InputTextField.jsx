165df56cc4e00c4fc770e341df74f62f50268622
yankie <yakielarapacheco@yahoo.com>
2022-01-17 19:55:03 -0500

    Adding InputTextField component

---
+++ src/components/InputTextField.jsx
@@ -0,0 +1,35 @@
+import {Field} from "react-final-form";
+import {TextField} from "@mui/material";
+import {required} from "./helpers";
+
+export const InputTextField = ({name, placeholder}) => {
+    return (
+        <Field name={name}
+               initialValue={''}
+               validate={required}
+               subscription={{
+                   error: true,
+                   touched: true,
+                   value: true
+               }}
+        >
+            {
+                ({input, meta}) => (
+                    <>
+                        <TextField label={placeholder}
+                                   color={'secondary'}
+                                   variant={'standard'}
+                                   style={{marginTop: '1rem'}}
+                                   id={name}
+                                   error={!!(meta.touched && meta.error)}
+                                   // helperText={meta.touched && meta.error ? 'Required': ''}
+                                   fullWidth
+                                   required
+                                   {...input}
+                        />
+                    </>
+                )
+            }
+        </Field>
+    )
+}
