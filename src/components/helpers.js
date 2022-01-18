dbe26ba8e03605da312090bb2bd189550fe81b26
yankie <yakielarapacheco@yahoo.com>
2022-01-17 20:16:19 -0500

    Adding form validator function

--- src/components/helpers.js
+++ src/components/helpers.js
@@ -1,5 +1,25 @@
 
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+// const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export const handleValidation = (values) => {
+    if (values['triggerThreshold'] === undefined)
+        window.alert('You must specify any trigger threshold')
+    else {
+        let maxPrev = 0
+        values['triggerThreshold'].forEach(({min, max}, index) => {
+            if (index === 0) maxPrev = max
+            if (parseInt(max) < 0 || parseInt(min) < 0)
+                window.alert('Threshold values must to be non-negative numbers')
+            if (parseInt(max) < parseInt(min))
+                window.alert('Maximum threshold value must to be greater than the minimum threshold value')
+            if (index !== 0) {
+                if (parseInt(min) < maxPrev)
+                    window.alert('Minimum threshold value must to be greater than the maximum threshold value of the previous item')
+                maxPrev = parseInt(max)
+            }
+        })
+    }
+}
 
 export const onSubmit = async (values) => {
     await sleep(300);
