c03a059d6bfbc9c9eba6e1f97bd73c28a9586aa1
yankie <yakielarapacheco@yahoo.com>
2022-01-17 20:02:18 -0500

    Adding TriggerThresholdList component

---
+++ src/components/TriggerThresholdList.jsx
@@ -0,0 +1,46 @@
+import {Button, List, ListItem, Typography} from "@mui/material";
+import {FieldArray} from "react-final-form-arrays";
+import {TriggerThresholdItem} from "./TriggerThresholdItem";
+import {Add} from "@mui/icons-material";
+
+
+export const TriggerThresholdList = ({name, add}) => {
+
+    return (
+        <>
+            <Typography variant={'body2'} style={{marginTop: '1rem'}}>
+                Define trigger threshold from the last date of the selected event
+            </Typography>
+
+            <List sx={{
+                width: '100%',
+                position: 'relative',
+                overflow: 'auto',
+                maxHeight: 200
+            }}
+            >
+                <FieldArray name={name} >
+                    {({ fields }) =>
+                        fields.map((name, index) => (
+                            <ListItem key={name}>
+                                <TriggerThresholdItem name={name}
+                                                      index={index}
+                                                      handleDelete={index => fields.remove(index)}
+                                />
+                            </ListItem>
+                        ))
+                    }
+                </FieldArray>
+            </List>
+
+            <Button variant={'outlined'}
+                    color={'secondary'}
+                    size={'large'}
+                    endIcon={<Add/>}
+                    style={{marginTop: '1rem'}}
+                    onClick={add}>
+                Add threshold
+            </Button>
+        </>
+    )
+}
